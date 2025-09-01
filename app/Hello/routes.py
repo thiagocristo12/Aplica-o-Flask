6844db938c0d68850de92b856d225e84bf921dd2
Thiago Cristo Dos Santos <thiago.cristods@gmail.com>
2025-09-01 20:16:33 -0300

    Update routes.py

--- app/Hello/routes.py
+++ app/Hello/routes.py
@@ -4,8 +4,9 @@ hello_bp = Blueprint("hello", __name__)
 
 @hello_bp.route("/")
 def index():
-    return "Hello, World"
+    return "òla, Mundo"
 
 @hello_bp.route("/sobre")
 def sobre():
     return "Óla, Thiago"
+
